d647cb522b54b5e1e076b42e67bc3aa02cc14871
Legendarycentaur <anton.backdahl@gmail.com>
2023-04-05 10:41:43 +0200

    Delete createAccount.js

--- createAccount.js
+++
@@ -1,6 +0,0 @@
-function createaccountmakeboxvisible(){
-    
-    document.getElementById("phonenumber").style.display="block";
-    document.getElementById("name").style.display="block";
-    document.getElementById("createAccount").style.display="none";
-}
